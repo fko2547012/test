a9253d416f1e4ef58e9e2ccf1aa0aa0c36e84a9e
fko2547012 <kayamaru.7193@gmail.com>
2026-06-24 09:41:37 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,59 @@
+package bean;
+
+public class Test {
+
+    private Student student;
+    private Subject subject;
+    private School school;
+    private String classNum;
+    private int no;
+    private Integer point;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+}
